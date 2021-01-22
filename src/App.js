cbc555d9fe82b0965d7a1caba89db41b93df6040
= <=>
2021-01-22 10:52:35 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import React, { Component } from 'react'
 
-class App extends Component {
-    
+export default class App extends Component {
+
+    state = {
+        astronauts: []
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(r => r.json())
+        .then(({people}) => this.setState({astronauts: people}))
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.astronauts.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+            </div>
+        )
+    }   
 }
